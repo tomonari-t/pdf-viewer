be5343dd734d707df53b9d9b5636d93cee4194b9
tomonari_t <tomo1208.japan@gmail.com>
2018-05-02 22:26:02 +0900

    add scss to react app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import PDFViewerComponent from '../PDFViewer/PDFViewer';
+
+
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <div>
+            Hello
+        </div>
         <PDFViewerComponent />
       </div>
     );
