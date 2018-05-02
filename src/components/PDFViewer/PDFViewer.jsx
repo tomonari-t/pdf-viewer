6f3b21f16ddef79617529a2ba673af730732a735
tomonari_t <tomo1208.japan@gmail.com>
2018-05-02 13:23:12 +0900

    implement text layer

--- src/components/PDFViewer/PDFViewer.jsx
+++ src/components/PDFViewer/PDFViewer.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import * as PDFJS from 'pdfjs-dist';
-import sample_1_pdf from '../../pdf/sample_1.pdf';
+import sample_1_pdf from '../../pdf/sample_2.pdf';
+import 'pdfjs-dist/web/pdf_viewer.css';
+const CMAP_URL = '/cmaps/';
 
 export default class PDFViewerComponent extends React.Component {
 
   renderPages = async (pdfDoc) => {
     for (let num = 1; num <= pdfDoc.numPages; num++) {
       const page = await pdfDoc.getPage(num);
-      const scale = 1.0;
+      const scale = 1.5;
       const canvasContaier = this.refs.canvasContainer;
+      const div = document.createElement('div');
+      div.setAttribute('id', `page-${page.pageIndex + 1}`);
+      div.setAttribute('style', 'position: relative; width: 80%; margin: 0 auto');
+      canvasContaier.appendChild(div);
+
       const canvas = document.createElement('canvas');
+      div.appendChild(canvas);
+
       const context = canvas.getContext('2d');
       const viewport = page.getViewport(scale);
       canvas.height = viewport.height;
@@ -18,19 +27,38 @@ export default class PDFViewerComponent extends React.Component {
         canvasContext: context,
         viewport: viewport
       };
-      canvasContaier.appendChild(canvas);
-      page.render(renderContext);
+      await page.render(renderContext);
+
+      // text-layer
+      const textContent = await page.getTextContent();
+      const textLayerDiv = document.createElement('div');
+      textLayerDiv.setAttribute('class', 'textLayer');
+      textLayerDiv.setAttribute('style', `width: ${viewport.width}px; height: ${viewport.height}px; margin: 0 auto;`)
+      div.appendChild(textLayerDiv);
+      PDFJS.renderTextLayer({
+        textContent,
+        container: textLayerDiv,
+        pageIndex: page.pageIndex,
+        viewport: viewport
+      });
+      // anotation-layer
     }
   }
 
   componentDidMount = async () => {
-    const pdfDoc = await PDFJS.getDocument(sample_1_pdf);
+    const pdfDoc = await PDFJS.getDocument({
+      url: sample_1_pdf,
+      cMapUrl: CMAP_URL,
+      cMapPacked: true,
+    });
     await this.renderPages(pdfDoc);
   }
 
   render() {
     return (
-      <div id="canvasContainer" ref={'pdf'} ref="canvasContainer">
+      <div
+        id="canvasContainer"
+        ref="canvasContainer">
       </div>
     );
   }
